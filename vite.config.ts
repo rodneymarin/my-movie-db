2fb6dc6489ec89b0b633cb469eae90dfbe49dbdd
Rodney <rodney.marin@gmail.com>
2025-01-18 18:33:55 -0400

    updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mymoviedb-public",
+  base: "/mymoviedb-build",
   build: {
-    outDir: '../../dist'
+    outDir: '../../build'
   }
 });
