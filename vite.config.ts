682e81465323ed42076076ca138310c4c77dec70
Rodney <rodney.marin@gmail.com>
2025-03-03 07:32:34 -0400

    update builds

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mymoviedb-build",
-  build: {
-    outDir: '../build'
-  }
+  base: "/mymoviedb",
 });
